d64b6852e991f7512aaf23107e3a70c8a3367db5
johnson378378 <111688010+johnson378378@users.noreply.github.com>
2024-05-24 09:00:58 +0800

    Update game.js

--- test/game.js
+++ test/game.js
@@ -2,7 +2,7 @@ function startGame() {
     const userInput = document.getElementById('userInput').value;
     const message = document.getElementById('message');
 
-    if (userInput.length !== 4 || !/^\d{4}$/.test(userInput)) {
+    if (userInput.length !== 3 || !/^\d{3}$/.test(userInput)) {
         message.innerText = '請輸入4位數字';
         return;
     }
@@ -11,11 +11,11 @@ function startGame() {
     const startTime = Date.now();
 
     function attemptCrack() {
-        const randomStr = generateRandomString(4, '0123456789');
+        const randomStr = generateRandomString(3, '0123456789');
         if (randomStr === userInput) {
             found = true;
             const elapsedTime = (Date.now() - startTime) / 1000;
-            message.innerText = `失敗了，僅需 ${elapsedTime.toFixed(2)} 秒就算出你的密碼是: ${userInput}。請輸入6位以上包含數字及英文大小寫的密碼。`;
+            message.innerText = `失敗了，僅需 ${elapsedTime.toFixed(2)} 秒就算出你的密碼是: ${userInput}。請輸入4位以上包含數字及英文大小寫的密碼。`;
         } else if (Date.now() - startTime >= 5000) {
             message.innerText = '恭喜，你的密碼強度夠高！這是答案Answer3 : A';
         } else {
