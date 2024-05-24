d53b536e658e0867438b4080b32fefe770bd136e
johnson378378 <111688010+johnson378378@users.noreply.github.com>
2024-05-24 14:37:33 +0800

    Update and rename PSgame.js to Pgame.js

--- test/Pgame.js
+++ test/Pgame.js
@@ -14,7 +14,7 @@ function startGame() {
         // 第二次要求輸入至少10位，且包含數字、英文大小寫及特殊符號的密碼
         message.innerText = '不錯!!!這密碼強度很夠，來吧給你答案 Answer3 : A';
     } else {
-        message.innerText = isSecondAttempt ? '請使用(輸入)10位以上包含數字+小寫英文+大寫英文+特殊符號的高強度密碼' : '請輸入7位數字密碼(純數字)';
+        message.innerText = isSecondAttempt ? '7位數字太容易被破解，請使用(輸入)10位以上包含數字+小寫英文+大寫英文+特殊符號的高強度密碼' : '請輸入7位數字密碼(純數字)';
     }
 }
 
@@ -32,7 +32,7 @@ function attemptCrack(userInput, length) {
             if (userHash === randomHash) {
                 const elapsedTime = Date.now() - startTime;
                 const elapsedTimeInSeconds = elapsedTime / 1000;
-                message.innerText = `慘了，我只花 ${elapsedTimeInSeconds.toFixed(5)} 破解出你的密碼: ${userInput}.請改成使用(輸入)10位以上包含數字+小寫英文+大寫英文+特殊符號的高強度密碼`;
+                message.innerText = `慘了，我只花 ${elapsedTimeInSeconds.toFixed(5)} 秒破解出你的密碼: ${userInput}.請改成使用(輸入)10位以上包含數字+小寫英文+大寫英文+特殊符號的高強度密碼`;
                 isSecondAttempt = true;
                 cracked = true;
                 return;
